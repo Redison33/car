d3c2a7d7a1b9579bb41e49fa7392f290abac512c
unknown <redison.ray3318@gmail.com>
2026-06-22 23:50:46 +0300

    Feature: добавдена страница с вложенной менюхой

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,32 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  function initHeaderNav() {
+    const nav = document.querySelector('.menu__nav');
+    if (!nav) return;
+
+    for (const link of nav.querySelectorAll('.list__item')) {
+      if (link.dataset.inited) continue;
+      link.dataset.inited = 'true';
+
+      link.addEventListener('click', (e) => {
+        const nested = link.querySelector('.nested');
+        if (!nested || e.target.classList.contains('nested__link') || e.target.tagName === 'SPAN')
+          return;
+
+        e.preventDefault();
+
+        const nestedEl = link.querySelector('.nested');
+        const svg = link.querySelector('svg');
+
+        if (nestedEl.style.height) {
+          nestedEl.style.height = '';
+          svg.style.transform = '';
+        } else {
+          nestedEl.style.height = nestedEl.scrollHeight + 'px';
+          svg.style.transform = 'rotate(180deg)';
+        }
+      });
+    }
+  }
+
+  initHeaderNav();
+});
