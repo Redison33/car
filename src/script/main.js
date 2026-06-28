d9c1a4d186dc4697a413f7456f9bd0b987e3680b
unknown <redison.ray3318@gmail.com>
2026-06-28 19:47:55 +0300

    Feature: Финиш

--- src/script/main.js
+++ src/script/main.js
@@ -16,13 +16,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const nestedEl = link.querySelector('.nested');
         const svg = link.querySelector('svg');
+        const text = link.querySelector('span');
 
         if (nestedEl.style.height) {
           nestedEl.style.height = '';
           svg.style.transform = '';
+          svg.querySelector('path').style.fill = '';
+          text.style.color = '';
         } else {
           nestedEl.style.height = nestedEl.scrollHeight + 'px';
           svg.style.transform = 'rotate(180deg)';
+          svg.querySelector('path').style.fill = '#0d3fa5';
+          text.style.color = '#0d3fa5';
         }
       });
     }
