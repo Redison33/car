a55a6cecf630eccb85b4f4347204a165e9796df4
unknown <redison.ray3318@gmail.com>
2026-06-29 23:36:13 +0300

    Fix: фикс бага модалок

--- src/script/main.js
+++ src/script/main.js
@@ -39,14 +39,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const modal = document.querySelector('.modal');
   const overlay = document.querySelector('.overlay');
 
-  modal.querySelector('.cancel').addEventListener('click', (e) => {
-    e.preventDefault();
-    modal.removeAttribute('style');
-    overlay.removeAttribute('style');
-  });
+  if (modal) {
+    modal.querySelector('.cancel').addEventListener('click', (e) => {
+      e.preventDefault();
+      modal.removeAttribute('style');
+      overlay.removeAttribute('style');
+    });
+  }
 
-  openModal.addEventListener('click', (e) => {
-    modal.style.display = 'block';
-    overlay.style.display = 'block';
-  });
+  if (openModal) {
+    openModal.addEventListener('click', (e) => {
+      modal.style.display = 'block';
+      overlay.style.display = 'block';
+    });
+  }
 });
